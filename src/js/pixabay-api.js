be92a7c184db7121a838362b5941371f41286c0e
Dumitru <dima50066@gmail.com>
2024-06-05 17:01:24 +0200

    rework project files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,23 @@
+import axios from 'axios';
+
 const API_KEY = '44238533-8b617bda18ddca61b92d6b256';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
-      query
-    )}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  if (!response.ok) {
+export async function fetchImages(query, page = 1, perPage = 15) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: perPage,
+      },
+    });
+    return response.data;
+  } catch (error) {
     throw new Error('Network response was not ok');
   }
-  return await response.json();
 }
--- src/main.js
+++ src/main.js
@@ -10,11 +10,15 @@ import {
 } from './js/render-functions.js';
 import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const gallery = document.getElementById('gallery');
 const loader = document.getElementById('loader');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let currentPage = 1;
 
 searchForm.addEventListener('submit', async event => {
   event.preventDefault();
@@ -42,9 +46,30 @@ searchForm.addEventListener('submit', async event => {
     }
 
     renderImages(images, gallery);
+    loadMoreBtn.style.display = 'block';
   } catch (error) {
     hideLoader(loader);
     showError('Something went wrong. Please try again later.');
+    console.error(error);
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage++;
+  try {
+    const data = await fetchImages(searchInput.value.trim(), currentPage);
+    renderImages(data.hits, gallery);
+
+    window.scrollBy({
+      top: gallery.getBoundingClientRect().height * 2,
+      behavior: 'smooth',
+    });
+
+    if (data.hits.length === 0 || data.totalHits <= currentPage * perPage) {
+      loadMoreBtn.style.display = 'none';
+      showInfo("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
     console.error(error);
   }
 });
